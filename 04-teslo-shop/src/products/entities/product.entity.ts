923b6774a4d5af9e167da88b6fa50ede1cdb9908
Alex Altuve <altuvealex6@gmail.com>
2025-02-03 14:46:16 -0400

    feat: rename database tables for product and product images to 'products' and 'products_images'

--- 04-teslo-shop/src/products/entities/product.entity.ts
+++ 04-teslo-shop/src/products/entities/product.entity.ts
@@ -1,7 +1,7 @@
 import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 import { ProductImage } from "./product-image.entity";
 
-@Entity()
+@Entity({'name': 'products'})
 export class Product {
     
     @PrimaryGeneratedColumn('uuid')
